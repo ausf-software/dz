b6e18ec662667f2906a8c4c3e5613951b6896701
Daniil Scherbina <77770487+ausf-software@users.noreply.github.com>
2022-10-16 18:10:45 +0300

    add

---
+++ kub16102022.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+
+int main()
+{
+
+    std::cout << "1" << std::endl << std::endl;
+    int a[20];
+    int n;
+    int max = 0;
+    std::cin >> n;
+    for (int i = 0; i < n; i++) 
+    {
+        std::cin >> a[i];
+        if (a[i] % 7 == 0)
+        {
+            if (i == 0) {
+                max = a[i];
+            }
+            if (a[i] > max)
+            {
+                max = a[i];
+            }
+        }
+    }
+    if (max != 0)
+    {
+        std::cout << max << std::endl;
+    }
+    else
+    {
+        std::cout << "no" << std::endl;
+    }
+
+    std::cout << "2" << std::endl << std::endl;
+    max = 0;
+    int c = 0;
+    std::cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> a[i];
+        if (a[i] >= 0)
+        {
+            max += a[i];
+            c++;
+        }
+    }
+    if (max != 0)
+    {
+        std::cout << float(max / c) << std::endl;
+    }
+    else
+    {
+        std::cout << "no" << std::endl;
+    }
+
+    std::cout << "3" << std::endl << std::endl;
+    max = 0;
+    std::cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> a[i];
+        if (a[i] >= 0)
+        {
+            max += a[i];
+        }
+    }
+    if (max != 0)
+    {
+        std::cout << max << std::endl;
+    }
+    else
+    {
+        std::cout << "no" << std::endl;
+    }
+
+
+    std::cout << "4" << std::endl << std::endl;
+    max = 0;
+    std::cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> a[i];
+        if (a[i] < 0)
+        {
+            if (i == 0) {
+                max = a[i];
+            }
+            if (a[i] < max)
+            {
+                max = a[i];
+            }
+        }
+    }
+    if (max != 0)
+    {
+        std::cout << max << std::endl;
+    } 
+    else
+    {
+        std::cout << "no" << std::endl;
+    }
+
+    
+}
