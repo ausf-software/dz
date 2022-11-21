81d324c342687e298fd03dc47275c21c4bbec7b4
Daniil Scherbina <77770487+ausf-software@users.noreply.github.com>
2022-11-21 18:46:09 +0300

    add

---
+++ kub21112022.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+
+bool isT(int a[], int n)
+{
+    int count = 0;
+    bool temp = false;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] % 3 == 0 && a[i] % 5 == 0)
+        {
+            count++;
+        }
+        if (count == 2)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool chet(int a)
+{
+    int sum = 0;
+    while (a)
+    {
+        sum += a % 10;
+        a /= 10;
+    }
+    return sum % 2 == 0;
+}
+
+bool sov(int a)
+{
+    int sum = 0;
+    for (int i = 1; i < a; i++)
+    {
+        if (a % i == 0)
+        {
+            sum += i;
+        }
+    }
+    return sum == a;
+}
+
+void z6()
+{
+    int n, m, count = 0;
+    std::cin >> n >> m;
+    for (int i = n; i <= m; i++)
+    {
+        if (chet(i)) count++;
+    }
+    std::cout << count;
+}
+
+void z7()
+{
+    int n, count = 0;
+    std::cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int temp;
+        std::cin >> temp;
+        if (sov(temp)) count++;
+    }
+    std::cout << float(count / n);
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
