c810335702d145337961226c2a12b86e108bf860
Daniil Scherbina <77770487+ausf-software@users.noreply.github.com>
2022-12-13 07:14:48 +0300

    add

---
+++ kub13122022.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+bool isDvuz(int a)
+{
+    return a < -9 && a > -100;
+}
+
+int main()
+{
+    int a = 1;
+    int b = 0;
+    int sum = 0;
+    while(a)
+    {
+        std::cin >> a;
+        if (b)
+        {
+            if (a % 2 == 0 && isDvuz(b))
+            {
+                sum += b * b;
+            }
+        }
+        b = a;
+    }
+    std::cout << sum;
+}
